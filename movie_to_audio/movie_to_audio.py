26c2afcfff3b3a1d612c9bce8abdf2858956f630
nyanyacyan <nyanyacyan@gmail.com>
2024-02-18 05:54:29 +0900

    mp4から音声データに変換ファイル作成完了

---
+++ movie_to_audio/movie_to_audio.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+# --------------------------------------------------------------------------------
+# mp４ から wavに変換するモジュール
+# 2023/2/18 制作
+
+#---バージョン---
+# Python==3.8.10
+
+# --------------------------------------------------------------------------------
+import os
+from dotenv import load_dotenv
+from moviepy.editor import AudioFileClip
+
+# 自作モジュール
+from my_decorators.logging_decorators import debug_logger_decorator
+
+load_dotenv()
+
+class Mp4ToWav:
+    @debug_logger_decorator
+    def mp4_to_wav(self):
+        movie_file = os.getenv('MP4_FILE')
+        # 音声データに置き換え
+        wav_file = AudioFileClip(movie_file)
+        # 音声データを保存
+        wav_file.write_audiofile(movie_file.replace('.mp4', '.wav'))
---
+++ movie_to_audio/youtube_url_to_wav.py
@@ -0,0 +1,39 @@
+# coding: utf-8
+# --------------------------------------------------------------------------------
+# 
+# 2023/2/18 制作
+
+#---バージョン---
+# Python==3.8.10
+
+# --------------------------------------------------------------------------------
+import os
+from dotenv import load_dotenv
+from yt_dlp import YoutubeDL
+
+# 自作モジュール
+from my_decorators.logging_decorators import debug_logger_decorator
+
+load_dotenv()
+
+class YoutubeToWav:
+    def __init__(self):
+        self.youtube_url = os.getenv('YOUTUBE_URL')
+        
+    @debug_logger_decorator
+    def youtube_to_wav(self):
+        # ydl_opts = {
+        #     'format': 'bestaudio/best',
+        #     'postprocessors': [{
+        #         'key': 'FFmpegExtractAudio',
+        #         'preferredcodec': 'wav',  # mp4に入れ替えれば動画になる
+        #         'preferredquality': '192',
+        #     }],
+        # }
+
+        ydl_opts = {
+            'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
+        }
+
+        with YoutubeDL(ydl_opts) as ydl:
+            ydl.download([self.youtube_url])
