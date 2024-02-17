5a3ed69862a4f669cf53c391e7925d9902b58c34
nyanyacyan <nyanyacyan@gmail.com>
2024-02-18 03:00:24 +0900

    エラー時のハンドリングのデコレーター作成。

--- decorators/error_handling_decorators.py
+++ decorators/error_handling_decorators.py
@@ -6,3 +6,31 @@
 #---バージョン---
 # Python==3.8.10
 # ----------------------------------------------------------------------------------
+import os
+from dotenv import load_dotenv
+import functools
+
+
+# 自作モジュール
+from logger.debug_logger import Logger
+
+load_dotenv()
+
+# logger初期化
+debug_mode = os.getenv('DEBUG_MODE', 'False') == 'True'
+logger_instance = Logger(__name__, debug_mode=debug_mode)
+logger = logger_instance.get_logger()
+
+# 呼び出す際には「 @debug_logger_decorator 」
+def error_handling_decorator(func):
+    @functools.wraps(func)
+
+    # *args=> 関数の引数に何を入れてもいい状態にする(位置引数)
+    # **kwargs=> 引数に特別な要素の指定することができる（キーワード引数=> 辞書含む）
+    def wrapper(*args, **kwargs):  # args=> 関数に何を入れてもいい状態にする
+        try:
+            result = func(*args, **kwargs)  # 下の関数を呼び出してる
+
+        except Exception as e:
+            logger.debug(f"{func.__name__}処理中にエラーが起きました: {e}")
+    return wrapper
