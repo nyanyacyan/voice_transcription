0ea8bc13d84abdec7c3d11d848a571928956216c
nyanyacyan <nyanyacyan@gmail.com>
2024-02-22 15:21:56 +0900

    ChatGPTクラス完成。 クラスの細分化して継承などしやすいように修正。

--- chatgpt/chatgpt_translator.py
+++ chatgpt/chatgpt_translator.py
@@ -8,68 +8,68 @@
 # ----------------------------------------------------------------------------------
 from openai import OpenAI
 import os
+import tiktoken
 from dotenv import load_dotenv
 import pandas as pd
 
 # 自作モジュール
-# from logger.debug_logger import Logger
-
-load_dotenv()
-
-API_Key = os.getenv("OPENAI_API_KEY")
+from logger.debug_logger import Logger
+from my_decorators.logging_decorators import debug_logger_decorator
 
 class ChatgptTranslator:
-    # def __init__(self, debug_mode=False):
-    #     # Loggerクラスを初期化
-    #     debug_mode = os.getenv('DEBUG_MODE', 'False') == 'True'
-    #     self.logger_instance = Logger(__name__, debug_mode=debug_mode)
-    #     self.logger = self.logger_instance.get_logger()
-    #     self.debug_mode = debug_mode
+    def __init__(self, api_key, debug_mode=False):
+        self.api_key = api_key
+        self.client = OpenAI(api_key=api_key)
 
+        # Loggerクラスを初期化
+        debug_mode = os.getenv('DEBUG_MODE', 'False') == 'True'
+        self.logger_instance = Logger(__name__, debug_mode=debug_mode)
+        self.logger = self.logger_instance.get_logger()
+        self.debug_mode = debug_mode
 
-    def chatgpt_translator(self):
-        client = OpenAI(api_key=API_Key)
+    @debug_logger_decorator
+    def read_file(self, file_path):
+        '''
+        文字起こしされたファイル読込
+        '''
+        with open(file_path, 'r', encoding='utf-8') as file:
+            return file.read()
 
-
-
-        # ここのファイルを複数読み込めるようにする
-        # 読み込むテキストを定義
-        with open('whisper_write_file.txt', 'r', encoding='utf-8') as file:
-            text = file.read()
-
-        # 翻訳指示ファイルを読み込む
-        translate_file = '翻訳指示ファイル.xlsx'
-        ja_translate_instruction = []
-
+    @debug_logger_decorator
+    def read_translation_instructions(self, translate_file):
+        '''
+        翻訳指示書を読込。全ての指示を１つに。
+        '''
         df = pd.read_excel(translate_file, usecols=['from', 'to'])
+        ja_translate = []
 
-        for index, row in df.iterrows():
+        for _, row in df.iterrows():
 
             translate_part = row['from']
             ja_part = row['to']
 
             instruction_part = f'\n文章「{translate_part}」は「{ja_part}」と和訳\n'
 
-            ja_translate_instruction.append(instruction_part)
+            ja_translate.append(instruction_part)
 
-        # 翻訳指示の内容を確認
-        # self.logger.info(ja_translate_instruction)
-        print(ja_translate_instruction)
+        return "".join(ja_translate)
 
-
-
-        res = client.chat.completions.create(
+    @debug_logger_decorator
+    def chatgpt_request(self, before_text_file, ja_translate):
+        '''
+        ChatGPTへの指示書（余計な文字をクリーン）
+        '''
+        res = self.client.chat.completions.create(
             # モデルを選択
-            model    = "gpt-3.5-turbo",
+            model = "gpt-3.5-turbo",
             
             # メッセージ
             messages  = [
                 {"role": "system", "content": f'You are a helpful assistant that translates to Japanese.'},
                 {"role": "user",
-                "content": f"添付したテキストファイルを全て文章を和訳して全てを表示ほしい。「{text}」\n「添付したテキストファイル」に下記の文章があった場合には必ず指定した和訳に置き換えてください。{ja_translate_instruction}\n上記で指定した和訳が、ちゃんと反映してるかを確認してるかな？。"},
+                "content": f"添付したテキストファイルを全て文章を和訳して全てを表示ほしい。「{before_text_file}」\n「添付したテキストファイル」に下記の文章があった場合には必ず指定した和訳に置き換えてください。{ja_translate}\n上記で指定した和訳が、ちゃんと反映してるかを確認してるかな？。"},
             ] ,
 
-            
             max_tokens  = 4096,             # 生成する文章の最大単語数
             n           = 1,                # いくつの返答を生成するか
             # stop        = None,             # 指定した単語が出現した場合、文章生成を打ち切る
@@ -80,12 +80,15 @@ class ChatgptTranslator:
         translate_text = res.choices[0].message.content
         clean_text = translate_text.replace(')', '')
 
-        print(clean_text)
+        return clean_text
+    
 
-
-        with open('chatgpt_write_file.txt', 'w', encoding='utf-8') as output_file:
-            output_file.write(clean_text)
-
-if __name__ == '__main__':
-    chatgpt_instance = ChatgptTranslator()
-    chatgpt_instance.chatgpt_translator()
+    @debug_logger_decorator
+    def chatgpt_translator(self, before_text_file_path, translate_file):
+        '''
+        メインメソッド
+        クラスの全てを並べ当てはめる
+        '''
+        before_text_file = self.read_file(before_text_file_path)
+        full_instructions = self.read_translation_instructions(translate_file)
+        self.chatgpt_request(before_text_file, full_instructions)
---
+++ data_division.py
@@ -0,0 +1,66 @@
+# coding: utf-8
+# ----------------------------------------------------------------------------------
+# loggerクラス
+# 2023/2/18 制作
+
+#---バージョン---
+# Python==3.8.10
+# ----------------------------------------------------------------------------------
+import tiktoken
+import os
+from transformers import GPT2Tokenizer
+
+
+def chatgpt_text_split_save():
+    file_path = "whisper_write_file.txt"
+    block_size = 3000  # このバーを超えたらテキストファイルを変える。少し余力を持ったものにする
+    separat_part = '\n'
+    output_dir = "/Users/nyanyacyan/Desktop/ProgramFile/project_file/voice_transcription/chatgpt/data_division_box"
+
+    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+
+    with open(file_path, 'r', encoding='utf-8') as f:
+        text = f.read()
+
+    # 一つ一つを区切ってるパーツによって分解
+    lines = text.split(separat_part)
+
+    # 各ボックスの初期化
+    blocks = []
+    block = ''
+    token_count = 0
+
+    for line in lines:
+        # lineにあるトークンをカウント
+        t = len(tokenizer.encode(line))
+
+        # トークンカウントが０以上（初期ではない）＋block_sizeを超えてしまったら新しくリストに追加する
+        if token_count > 0 and block_size < (token_count + t):
+            blocks.append(block)
+            token_count = 0
+            block = ''
+        
+        # トークンカウントを追加
+        token_count += t
+
+        # ブロックに区切れた部分を追加する（区切られたもの自体も追加）
+        block += line + separat_part
+
+    # 最後のblock_sizeに達してないものも追加（ここまでのものは全てblocksに追加されてるためない）
+    if block:
+        blocks.append(block)
+
+
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        for i, block in enumerate(blocks):
+            output_text = os.path.join(output_dir, f'text_block_{i+1}.txt')
+            with open(output_text, 'w', encoding='utf-8') as output_file:
+                output_file.write(block)
+
+            print(f"{output_text} 保存完了")
+
+
+if __name__ == '__main__':
+    chatgpt_text_split_save()
